63e7e5c6a2bfa572fa565919b3943740548c4ab7
David Shulkin <davidshulkin@Davids-iMac.attlocal.net>
2024-04-04 20:55:47 -0700

    Storing History & Lifting state into Game()

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,7 @@ function Square({value, onSquareClick}) {
   )
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return
@@ -23,8 +20,7 @@ function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext) 
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares)
@@ -63,7 +59,8 @@ export default function Game() {
   const currentSquares = history[history.length - 1]
 
   function handlePlay(nextSquares) {
-
+    setHistory([...history, nextSquares])
+    setXIsNext(!xIsNext)
   }
 
   return (
@@ -109,6 +106,12 @@ export default function Game() { }
 This tells your index.js file to use the Game component as the top-level component 
 instead of your Board component.
 
+---- The Game() component is the new top level component. It will store the history of each square array. 
+
+----- NOTE: Just like the square component is lifted into the Board() component, we will lift state into the Game() component.
+
+----- This gives the Game component full control over the Board’s data and lets it instruct the Board to render previous turns from the history.
+
 
 const [xIsNext, setXIsNext] = useState(true);
 Tracks which player is next.
@@ -126,14 +129,27 @@ of 9 elements representing the 9 squares (9 null values).
 ---- history is the current state value. It starts as [Array(9).fill(null)], which is an array containing a single element 
      that is an array of 9 null values. This represents the initial state of the game board.
 
+     ---- Were storing each past version of the squares arrary
+          The history array represents all board states, from the first to the last move, and has a shape like this:
+
+          [
+          // Before first move
+          [null, null, null, null, null, null, null, null, null],
+
+          // After first move
+          [null, null, null, null, 'X', null, null, null, null],
+
+          // After second move
+          [null, null, null, null, 'X', null, null, null, 'O'],
+          // ...
+        ]
+
 ---- setHistory is a function that allows you to update history. When you call setHistory with a new value, it updates history 
      to that new value and re-renders the component with the updated state.
 
 
 history[history.length - 1]
-This expression accesses the last element in the history array, which represents the 
-current state of the game. Each move by a player is recorded  as a new state in 
-the history, allowing the game to know the current configuration of the game board.
+Renders the squares for the current move, by reading the last squares array from the history.
 
 ---- This part calculates the index of the last element in the history array.
 ---- The square brackets [ ] are used for accessing an element within the history array. 
@@ -145,4 +161,33 @@ Renders the squares for the current move by reading the last squares array from 
 The current state of the game board is assigned to the currentSquares constant. 
 This variable holds the array that represents the current layout 
 of the Tic-Tac-Toe board.
+
+
+function Board({xIsNext, squares, onPlay})}
+Here the Board() component is controlled by the three props it recieves.
+The Board() will call the new onPlay function with the updated squares array when a player makes a move.
+
+---- Notice the first two lines of the Board function that call useState have been removed.
+     Because that has been done, we are also replacing the setSquares and SetXIsNext call in the handClick in 
+     the Board() with a single call to the onPlay function so that Game() can update the Board when a user
+     clicks a square.
+---- The Board passes the updated squares array to onPlay
+
+
+function handlePlay(nextSquares) { }
+The handlePlay function in the Game() updates the Game’s state by triggerering a re-render.
+
+
+[...history, nextSquares] 
+creates a new array that contains all the items in history, followed by nextSquares.
+For example, if history is [[null,null,null], ["X",null,null]] and nextSquares is ["X",null,"O"], 
+then the new [...history, nextSquares] array will be [[null,null,null], ["X",null,null], ["X",null,"O"]]
+
+
+setXIsNext(!xIsNext)
+toggles xIsNext like the Board used to do.
+
+
+At this point, state is moved to live in the Game component.
+     
 */
